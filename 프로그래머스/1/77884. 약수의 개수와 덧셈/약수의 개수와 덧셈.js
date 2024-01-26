7c2e82ec5a6dcbd540a5df1d1dd634125abaa93e
HA0N1 <154482801+HA0N1@users.noreply.github.com>
2024-01-26 09:50:08 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,17 @@
+// 주어진 두 숫자 사이의 숫자 구하기
+// 숫자들의 약수의 갯수 구하기
+// 제곱근이 정수 O : 약수의 개수는 홀수
+// 제곱근이 정수 X : 약수의 개수는 짝수
+// 약수의 갯수가 짝수면 + 홀수면 -
+// 총 값 리턴
+function solution(left, right) {
+    let btweenNum = [];
+    let result
+    let sum = 0
+    for (i = left; i <= right; i++) {
+        btweenNum.push(i);
+    }
+ result = btweenNum.map((num)=>Number.isInteger(Math.sqrt(num))?  sum-=num : sum+=num)
+    return result.pop()
+}
+
