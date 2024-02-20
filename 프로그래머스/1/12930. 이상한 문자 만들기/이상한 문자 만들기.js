97f001ccc598a87cea619884d926bb83f93b766b
HA0N1 <154482801+HA0N1@users.noreply.github.com>
2024-02-20 09:43:53 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,19 @@
+// 짝수 번 째 대문자 홀수 번 째 소문자
+function solution(s) {
+    let answer = '';
+    let words = s.split(' ');
+    
+    for (let i = 0; i < words.length; i++) {
+        for(let j = 0; j < words[i].length; j++) {
+            if(j % 2 === 0) {
+                answer += words[i][j].toUpperCase();
+            } else {
+                answer += words[i][j].toLowerCase();
+            }
+        }
+        if (i < words.length -1) {
+            answer += ' ';
+        }
+    }
+    return answer;
+}
