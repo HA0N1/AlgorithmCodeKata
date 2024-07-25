ddc0576be6098a259ba42a1fc867b6e9130d9da9
Haon Choi <intor48@gmail.com>
2024-07-25 15:30:51 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
@@ -1,4 +1,3 @@
 function solution(num_str) {
-    let answer = num_str.split('').map(Number).reduce((accumulator, currentValue) => accumulator + currentValue,0)
-    return answer;
+    return [...num_str].reduce((accumulator, currentValue) => accumulator + +currentValue,0)
 }
