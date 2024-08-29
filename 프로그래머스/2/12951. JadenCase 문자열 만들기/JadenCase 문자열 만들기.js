3e1345866cb1373f37ef87843ec2802d54d118c3
Haon Choi <intor48@gmail.com>
2024-08-30 07:30:13 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,15 @@
+// 공백 이후 문자 대문자로 그 외 소문자로
+// toUpperCase, toLowerCase
+function solution(s) {
+    let words = s.toLowerCase().split(' ');
+    
+    let jadenCase = words.map(word => {
+        if (word.length > 0) {
+            return word[0].toUpperCase() + word.slice(1);
+        }
+        return word; // 빈 문자열 처리
+    });
+    return jadenCase.join(' ');
+}
+
+console.log("공백 test",solution(" people Unfollowed Me"))
