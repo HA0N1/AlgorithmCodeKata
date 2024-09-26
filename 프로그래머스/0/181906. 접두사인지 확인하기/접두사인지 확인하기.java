8a7547199dd27139af3615841dffba9d577a72c5
Haon Choi <intor48@gmail.com>
2024-09-26 15:45:32 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.07 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        answer = my_string.indexOf(is_prefix);
+        if (answer == 0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
