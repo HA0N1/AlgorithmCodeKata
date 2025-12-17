16d5253e402a0d4288af02fad17702f45906fe12
Haon Choi <intor48@gmail.com>
2025-12-17 13:27:51 +0900

    [Bronze I] Title: 평균, Time: 92 ms, Memory: 9728 KB -BaekjoonHub

--- node.js/백준/Bronze/1546. 평균/평균.js
+++ node.js/백준/Bronze/1546. 평균/평균.js
@@ -6,18 +6,14 @@ const scores = nums.slice(1); // 길이 n
 /** 
 * 1. 최대 점수 찾기 => maxScore
 * 2. 모든 요소에 *80/100 => score
-* 3. totalScore += score
-* 4. totalScore / cnt
+* 3. total += score
+* 4. total / cnt
 */
 
-let max = 0
-let totalScore = 0
-for (const s of scores){ if(max < s) max = s}
+let max = 0;
+for (const s of scores) if (s > max) max = s;
 
-const cal = (num, max) => (num / max) * 100;
+let total = 0;
+for (const s of scores) total += (s / max) * 100;
 
-for (let s of scores) {
-    totalScore += cal(s, max);
-}
-
-console.log(totalScore / n)
+console.log(total / n);
